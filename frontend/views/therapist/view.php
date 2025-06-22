c88234242c71502ab12167be4dbed4b42397e80d
fanovi <63197488+fanovi@users.noreply.github.com>
2025-06-22 20:42:23 +0200

    Update therapist action button styles: Change delete button background color to error-500 and adjust hover state for consistency. Modify update button background color to brand-500 with a new hover state of brand-950 for improved visibility.

--- frontend/views/therapist/view.php
+++ frontend/views/therapist/view.php
@@ -56,14 +56,14 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php if (Yii::$app->user->can('update_therapist')): ?>
                 <?= Html::a('<svg class="mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"></path></svg>Modifica Terapista', 
                     ['update', 'id' => $model->id], [
-                    'class' => 'inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-brand-600 border border-transparent rounded-lg hover:bg-brand-700 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2'
+                    'class' => 'inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-brand-500 border border-transparent rounded-lg hover:bg-brand-950 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2'
                 ]) ?>
             <?php endif; ?>
             
             <?php if (Yii::$app->user->can('delete_therapist')): ?>
                 <?= Html::a('<svg class="mr-2 h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path></svg>Elimina', 
                     ['delete', 'id' => $model->id], [
-                    'class' => 'inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2',
+                    'class' => 'inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-error-500 border border-transparent rounded-lg hover:bg-error-500 focus:outline-none focus:ring-2 focus:ring-error-500 focus:ring-offset-2',
                     'data' => [
                         'confirm' => 'Sei sicuro di voler eliminare questo terapista?',
                         'method' => 'post',
