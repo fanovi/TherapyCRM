8d68da0d215ccb96c95baa8da5effb7187fd0435
fanovi <63197488+fanovi@users.noreply.github.com>
2025-08-20 19:35:38 +0200

    favicon login

--- frontend/views/layouts/blank.php
+++ frontend/views/layouts/blank.php
@@ -3,6 +3,7 @@
 /** @var \yii\web\View $this */
 /** @var string $content */
 
+use yii\helpers\Url;
 use frontend\assets\AppAsset;
 use yii\helpers\Html;
 
@@ -18,7 +19,9 @@ AppAsset::register($this);
         name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="icon" href="favicon.ico">
+    <link rel="icon" href="<?= Url::to('@web/images/logo/cropped-flavicon-32x32.png') ?>" sizes="32x32" />
+    <link rel="icon" href="<?= Url::to('@web/images/logo/cropped-flavicon-192x192.png') ?>" sizes="192x192" />
+    <link rel="apple-touch-icon" href="<?= Url::to('@web/images/logo/cropped-flavicon-180x180.png') ?>" />
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
