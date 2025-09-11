88b5cc801a1239b0d6bf3241bfa9ea58052d43e1
fanovi <63197488+fanovi@users.noreply.github.com>
2025-09-11 09:51:12 +0200

    [fix] Rimosso l'etichetta dal checkbox "Ha Autorità Genitoriale" nella vista di creazione credenziali per migliorare la chiarezza e l'usabilità del form.

--- frontend/views/patient/create-credentials.php
+++ frontend/views/patient/create-credentials.php
@@ -243,7 +243,7 @@ $this->params['breadcrumbs'][] = $this->title;
                         'class' => 'w-4 h-4 text-brand-600 bg-gray-100 border-gray-300 rounded focus:ring-brand-500 dark:focus:ring-brand-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600',
                         'uncheck' => '0',
                         'value' => '1'
-                    ])->label('Ha Autorità Genitoriale') ?>
+                    ])->label(false) ?>
                     <p class="mt-1 text-xs text-gray-500 dark:text-gray-400">
                         Seleziona se questa persona ha l'autorità genitoriale sul paziente.
                     </p>
