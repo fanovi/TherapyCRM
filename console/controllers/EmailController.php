107acfb15967e7ca2964a9000629713f85d26c81
fanovi <63197488+fanovi@users.noreply.github.com>
2026-01-08 11:22:01 +0100

    [update] Aggiunto un nuovo metodo di test nel controller EmailController per inviare email tramite SMTP, configurando l'involucro e l'indirizzo del mittente, e rinominato il metodo di test esistente.

--- console/controllers/EmailController.php
+++ console/controllers/EmailController.php
@@ -9,6 +9,9 @@ use common\models\Appointment;
 use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use yii\db\Expression;
 use yii\helpers\Console;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mime\Address;
+
 
 /**
  * Test email controller
@@ -18,7 +21,7 @@ use yii\helpers\Console;
 class EmailController extends Controller
 {
 
-    public function actionTest()
+    public function actionTest2()
     {
         try {
             $message = Yii::$app->mailer->compose();
@@ -28,6 +31,32 @@ class EmailController extends Controller
             $message->setTextBody('Test body');
             // :fuoco: FONDAMENTALE
             $message->setEnvelopeFrom('noreply@sanlucacentromedico.it');
+            $sent = $message->send();
+            var_dump($sent);
+        } catch (\Throwable $e) {
+            echo "ERRORE:\n";
+            echo $e->getMessage() . PHP_EOL;
+            echo $e->getTraceAsString();
+        }
+        return ExitCode::OK;
+    }
+
+    public function actionTest()
+    {
+        try {
+            $message = Yii::$app->mailer->compose()
+                ->setFrom('noreply@test.it')
+                ->setTo('vito.fasano@badil.it')
+                ->setSubject('SMTP ATOMIC TEST')
+                ->setTextBody('Test body');
+            // :fuoco: Envelope SMTP reale
+            $symfonyMessage = $message->getSymfonyMessage();
+            $symfonyMessage->setEnvelope(
+                new Envelope(
+                    new Address('noreply@test.it'),
+                    [new Address('vito.fasano@badil.it')]
+                )
+            );
             $sent = $message->send();
             var_dump($sent);
         } catch (\Throwable $e) {
