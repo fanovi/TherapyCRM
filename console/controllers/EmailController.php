2e500c21cecfa744d2a049e93161546cbd03ebee
fanovi <63197488+fanovi@users.noreply.github.com>
2026-01-08 11:23:36 +0100

    [update] Ristrutturato il controller EmailController per gestire l'invio di email con parametri semplificati e output di conferma, rimuovendo il metodo di test precedente e migliorando la gestione degli indirizzi email.

--- console/controllers/EmailController.php
+++ console/controllers/EmailController.php
@@ -6,63 +6,28 @@ use Yii;
 use yii\console\Controller;
 use yii\console\ExitCode;
 use common\models\Appointment;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 use yii\db\Expression;
 use yii\helpers\Console;
-use Symfony\Component\Mailer\Envelope;
-use Symfony\Component\Mime\Address;
-
 
 /**
- * Test email controller
+ * Gestisce il completamento automatico degli appuntamenti
  * 
- * @author Vito Fasano
+ * @author Your Name
  */
-class EmailController extends Controller
-{
-
-    public function actionTest2()
-    {
-        try {
-            $message = Yii::$app->mailer->compose();
-            $message->setFrom('noreply@sanlucacentromedico.it');
-            $message->setTo('vito.fasano@badil.it');
-            $message->setSubject('SMTP ATOMIC TEST');
-            $message->setTextBody('Test body');
-            // :fuoco: FONDAMENTALE
-            $message->setEnvelopeFrom('noreply@sanlucacentromedico.it');
-            $sent = $message->send();
-            var_dump($sent);
-        } catch (\Throwable $e) {
-            echo "ERRORE:\n";
-            echo $e->getMessage() . PHP_EOL;
-            echo $e->getTraceAsString();
-        }
-        return ExitCode::OK;
-    }
+class EmailController extends Controller {
 
     public function actionTest()
     {
-        try {
-            $message = Yii::$app->mailer->compose()
-                ->setFrom('noreply@test.it')
-                ->setTo('vito.fasano@badil.it')
-                ->setSubject('SMTP ATOMIC TEST')
-                ->setTextBody('Test body');
-            // :fuoco: Envelope SMTP reale
-            $symfonyMessage = $message->getSymfonyMessage();
-            $symfonyMessage->setEnvelope(
-                new Envelope(
-                    new Address('noreply@test.it'),
-                    [new Address('vito.fasano@badil.it')]
-                )
-            );
-            $sent = $message->send();
-            var_dump($sent);
-        } catch (\Throwable $e) {
-            echo "ERRORE:\n";
-            echo $e->getMessage() . PHP_EOL;
-            echo $e->getTraceAsString();
+        $email_sended = Yii::$app->mailer->compose()
+        ->setTo('vito.fasano@badil.it')
+        ->setFrom([Yii::$app->params['senderEmail'] => Yii::$app->params['senderName']])
+        ->setSubject('Test email')
+        ->setTextBody('Test email body')
+        ->send();
+        if ($email_sended) {
+            Console::output('Email sended');
+        } else {
+            Console::output('Email not sended');
         }
         return ExitCode::OK;
     }
