d59a8de3582fc354ded8d17d2ea83a475befb533
marco.rispoli <marco.rispoli@managify.it>
2026-03-03 14:55:37 +0100

    feat: add "Esporta Permessi" menu item with icon and routes for permissions export

--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -47,6 +47,12 @@ $menuItems = [
         'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />',
         'routes' => ['system-setting/calendar', 'system-setting/update-calendar'],
     ],
+    [
+        'label' => 'Esporta Permessi',
+        'url' => ['/export-permissions/index'],
+        'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />',
+        'routes' => ['export-permissions/index'],
+    ],
 ];
 ?>
 <?php $this->beginPage() ?>
