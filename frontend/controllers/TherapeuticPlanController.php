f7b1bb2d516193b847b3dcd7199c92f50bf99212
fanovi <vito.fasano87@gmail.com>
2026-05-25 12:36:21 +0200

    fix(TherapeuticPlanController): migliorare la ricerca di pazienti con CONCAT
    Aggiunta una nuova condizione alla query di ricerca per i pazienti, utilizzando CONCAT per combinare il cognome e il nome in entrambi gli ordini. Questo cambiamento migliora la flessibilità e l'accuratezza delle ricerche nel piano terapeutico.

--- frontend/controllers/TherapeuticPlanController.php
+++ frontend/controllers/TherapeuticPlanController.php
@@ -848,7 +848,8 @@ class TherapeuticPlanController extends BaseController
                     ['like', 'first_name', $q],
                     ['like', 'last_name', $q],
                     ['like', 'fiscal_code', $q],
-                    ['like', "CONCAT(first_name, ' ', last_name)", $q]
+                    ['like', "CONCAT(first_name, ' ', last_name)", $q],
+                    ['like', "CONCAT(last_name, ' ', first_name)", $q]
                 ])
                 ->orderBy(['last_name' => SORT_ASC, 'first_name' => SORT_ASC])
                 ->limit(20)
